25bf50d8b60f6f049d6fba46a7736fcbe0f2be64
maifeeulasad <maifeeulasad@gmail.com>
2021-06-09 23:40:15 +0600

    display icon

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { app } from 'electron'
-import { createWindow } from './src/Window';
+import { createWindow } from './src/window';
 
 app.on('ready', createWindow);
 app.on('window-all-closed', () => app.quit());
--- src/window.js
+++ src/window.js
@@ -1,4 +1,5 @@
 const { app , screen, BrowserWindow } = require('electron')
+const path = require('path');
 
 const url = 'https://web.whatsapp.com/';
 const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36';
@@ -7,6 +8,7 @@ export function createWindow() {
     const window = new BrowserWindow({
         width: screen.width,
         height: screen.height,
+        icon:path.join(__dirname, '../resources/icon48.png'),
         webPreferences: {
             devTools: false,
         },
