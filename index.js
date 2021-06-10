d2987f27542c7850e51ee4d1a0b1a5bf53c147e8
maifeeulasad <maifeeulasad@gmail.com>
2021-06-10 13:58:54 +0600

    more modular, //refactor : require to import

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { app } = require('electron')
+import { app as application } from 'electron';
 import { createWindow } from './src/window';
 import { createTray } from './src/tray';
+
+const app = application
 
 function start(){
     createWindow().then((window)=>{
