b9219f2743173ce557269a4122f00f4776e51b14
Maifee Ul Asad <maifeeulasad@gmail.com>
2021-07-27 21:23:27 +0600

    fix #4
     + on second instance-> just focus, don't create an exact instance
     + on failure singleton-lock-> show notification

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const {app} = require('electron')
+const {app, Notification} = require('electron')
+const path = require('path')
+
 const {createWindow} = require('./src/window')
 const {createTray}  = require('./src/tray')
 
+const lock = app.requestSingleInstanceLock()
+
+let windowSingleton
+
 const start = () => {
     createWindow().then((window)=>{
+        windowSingleton = window
         createTray(window,app)
     })
 }
 
+const showNotification = () => {
+    let icon = path.join(__dirname, './resources/icon48.png')
+    let title = "WhatApp"
+    let body = "Couldn't achieve SingleInstanceLock,\n" +
+        "so there may be more than one instance created!\n" +
+        "In that case, you have to close them manually"
+    const notification={
+        title,
+        body,
+        icon
+    }
+    new Notification(notification).show()
+}
+
+if(!lock){
+    showNotification()
+}
+
 app.on('ready', start)
+app.on('second-instance', () => {
+    if(windowSingleton){
+        windowSingleton.focus()
+    }
+})
 app.on('window-all-closed', () => app.quit())
